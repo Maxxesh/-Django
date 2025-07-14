079f19768348855cd3b0a61554bb2649d4cc555f
Maxxesh <123192637+Maxxesh@users.noreply.github.com>
2025-07-14 21:31:21 +0200

    Delete home/vitmaxesh directory

--- home/vitmaxesh/gallery/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'gallery'
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('upload/', views.upload_photo, name='upload'),
-    path('photo/<int:pk>/', views.view_photo, name='photo_detail'),
-    path('photo/<int:pk>/like/', views.like_photo, name='like_photo'),
-]
